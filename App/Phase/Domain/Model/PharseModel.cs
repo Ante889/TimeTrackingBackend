b5105118cf7bdc477450e5960f3bd791ad587d12
ante <ante.filipovic72@gmail.com>
2024-08-07 14:20:23 +0200

    Models

---
+++ App/Phase/Domain/Model/PharseModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimeTracking.App.Phase.Domain.Model
+{
+    public class PhaseModel
+    {
+        [Required(ErrorMessage = "Project is required")]
+        public int Project { get; set; }
+
+        [Required(ErrorMessage = "Date is required")]
+        public DateTime DateCreated { get; set; }
+
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Phase number is required")]
+        public int PhaseNumber { get; set; }
+
+        public decimal? AmountPaid { get; set; }
+    }
+}
---
+++ App/Time/Domain/Model/TimeModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimeTracking.App.Time.Domain.Model
+{
+    public class TimeModel
+    {
+        [Required(ErrorMessage = "Category is required")]
+        public int Category { get; set; }
+
+        [Required(ErrorMessage = "Time is required")]
+        public int TimeInMinutes { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(255, MinimumLength = 1, ErrorMessage = "Title must be between 1 and 255 characters")]
+        public required string Title { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
